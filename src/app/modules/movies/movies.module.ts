979a5a14a09219d02d981943305bfb0c7e092c1c
Fabian Forero <fabianforero779@gmail.com>
2022-06-04 09:46:47 -0400

    Add: include material elements on movies module
    Import material elements on movies module

--- src/app/modules/movies/movies.module.ts
+++ src/app/modules/movies/movies.module.ts
@@ -6,6 +6,7 @@ import { MoviesRoutingModule } from './movies-routing.module';
 import { MoviesComponent } from './movies.component';
 import { MovieCardComponent } from './components/movie-card/movie-card.component';
 import { MovieSearchComponent } from './components/movie-search/movie-search.component';
+import { MaterialModule } from '@material/material.module';
 
 @NgModule({
   declarations: [MoviesComponent, MovieCardComponent, MovieSearchComponent],
@@ -14,6 +15,7 @@ import { MovieSearchComponent } from './components/movie-search/movie-search.com
     FormsModule,
     ReactiveFormsModule,
     MoviesRoutingModule,
+    MaterialModule,
   ],
 })
 export class MoviesModule {}
