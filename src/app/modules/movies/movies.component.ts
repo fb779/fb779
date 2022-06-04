9f67ba0239f8840ab6580a94e3065114cdb8d0f7
Fabian Forero <fabianforero779@gmail.com>
2022-06-04 12:06:11 -0400

    Add: Definition of template for movies
    Definition of visual elements
        => serach input
        => list cards
        => card movie

--- src/app/modules/movies/movies.component.ts
+++ src/app/modules/movies/movies.component.ts
@@ -3,13 +3,12 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-movies',
   templateUrl: './movies.component.html',
-  styleUrls: ['./movies.component.scss']
+  styleUrls: ['./movies.component.scss'],
 })
 export class MoviesComponent implements OnInit {
+  listMovies: any = [{}, {}, {}, {}, {}, {}, {}, {}];
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
--- src/app/modules/movies/movies.module.ts
+++ src/app/modules/movies/movies.module.ts
@@ -1,12 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-
+import { MaterialModule } from '@material/material.module';
 import { MoviesRoutingModule } from './movies-routing.module';
+
 import { MoviesComponent } from './movies.component';
 import { MovieCardComponent } from './components/movie-card/movie-card.component';
 import { MovieSearchComponent } from './components/movie-search/movie-search.component';
-import { MaterialModule } from '@material/material.module';
 
 @NgModule({
   declarations: [MoviesComponent, MovieCardComponent, MovieSearchComponent],
