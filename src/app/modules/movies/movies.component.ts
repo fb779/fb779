d7ae0a31fe9c215cdb633280bd7a25012256717a
Fabian Forero <fabianforero779@gmail.com>
2022-07-26 10:50:11 -0400

    Add: Adjusts of movie component and implementation

--- src/app/modules/movies/movies.component.ts
+++ src/app/modules/movies/movies.component.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { NumberInput } from '@angular/cdk/coercion';
+import { PageEvent } from '@angular/material/paginator';
+
+import { MovieService } from '@movies/services/movie.service';
+import { Movie } from '@movies/models';
 
 @Component({
   selector: 'app-movies',
@@ -6,9 +12,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./movies.component.scss'],
 })
 export class MoviesComponent implements OnInit {
-  listMovies: any = [{}, {}, {}, {}, {}, {}, {}, {}];
+  listMovies$: Observable<Movie[]> = this.movieService.listMovies$;
+  searchTerm$: Observable<String> = this.movieService.searchTem$;
 
-  constructor() {}
+  totalMovies$: Observable<NumberInput> = this.movieService.totalMovies$;
+  actualPage$: Observable<NumberInput> = this.movieService.actualPage$;
+
+  constructor(private movieService: MovieService) {}
 
   ngOnInit(): void {}
+
+  searchChange(term: String) {
+    this.movieService.serachTerm = term;
+  }
+
+  changePageSize(pageEvent: PageEvent) {
+    this.movieService.changePage = pageEvent.pageIndex;
+  }
 }
--- src/app/modules/movies/services/movie.service.ts
+++ src/app/modules/movies/services/movie.service.ts
@@ -1,81 +1,85 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import {
-  BehaviorSubject,
-  combineLatest,
-  map,
-  Observable,
-  shareReplay,
-  switchMap,
+    BehaviorSubject,
+    combineLatest,
+    map,
+    Observable,
+    shareReplay,
+    switchMap,
 } from 'rxjs';
 import { MovieResponse, Movie } from '@movies/models';
 import { NumberInput } from '@angular/cdk/coercion';
 
 import {
-  API_URL_BASE,
-  API_KEY,
-  DEFAULT_SEARCH_TERM,
+    API_URL_BASE,
+    API_KEY,
+    DEFAULT_SEARCH_TERM,
 } from '@movies/services/movies-config';
 
 @Injectable({
-  providedIn: 'root',
+    providedIn: 'root',
 })
 export class MovieService {
-  private searchTermBS: BehaviorSubject<String> = new BehaviorSubject<String>(
-    ''
-  );
-  private actualPageBS: BehaviorSubject<number> = new BehaviorSubject<number>(
-    0
-  );
-
-  constructor(private http: HttpClient) {}
-
-  get searchTem$(): Observable<String> {
-    return this.searchTermBS.asObservable();
-  }
-
-  get actualPage$(): Observable<number> {
-    return this.actualPageBS.asObservable();
-  }
-
-  get moviesResponse$(): Observable<MovieResponse> {
-    return combineLatest([this.searchTem$, this.actualPage$]).pipe(
-      switchMap((params) => this.http.get<MovieResponse>(this.getUrl(params))),
-      shareReplay()
+    private searchTermBS: BehaviorSubject<String> = new BehaviorSubject<String>(
+        ''
     );
-  }
-
-  get listMovies$(): Observable<Movie[]> {
-    return this.moviesResponse$.pipe(
-      map<MovieResponse, Movie[]>(({ Search }: MovieResponse) =>
-        Search !== undefined ? Search : []
-      )
+    private actualPageBS: BehaviorSubject<number> = new BehaviorSubject<number>(
+        0
     );
-  }
 
-  get totalMovies$(): Observable<NumberInput> {
-    return this.moviesResponse$.pipe(
-      map<MovieResponse, number>(({ totalResults }: MovieResponse) =>
-        totalResults !== undefined ? totalResults : 0
-      )
-    );
-  }
+    constructor(private http: HttpClient) {}
 
-  set serachTerm(term: String) {
-    if (this.searchTermBS.value != term) {
-      this.actualPageBS.next(0);
+    get searchTem$(): Observable<String> {
+        return this.searchTermBS.asObservable();
     }
-    this.searchTermBS.next(term);
-  }
 
-  set changePage(page: number) {
-    this.actualPageBS.next(page);
-  }
+    get actualPage$(): Observable<number> {
+        return this.actualPageBS.asObservable();
+    }
 
-  private getUrl(params: any) {
-    const [searchTerm, actualPage] = params;
-    const term = searchTerm || DEFAULT_SEARCH_TERM;
+    get moviesResponse$(): Observable<MovieResponse> {
+        return combineLatest([this.searchTem$, this.actualPage$]).pipe(
+            switchMap((params) =>
+                this.http.get<MovieResponse>(this.getUrl(params))
+            ),
+            shareReplay()
+        );
+    }
 
-    return `${API_URL_BASE}?apikey=${API_KEY}&page=${actualPage + 1}&s=${term}`;
-  }
+    get listMovies$(): Observable<Movie[]> {
+        return this.moviesResponse$.pipe(
+            map<MovieResponse, Movie[]>(({ Search }: MovieResponse) =>
+                Search !== undefined ? Search : []
+            )
+        );
+    }
+
+    get totalMovies$(): Observable<NumberInput> {
+        return this.moviesResponse$.pipe(
+            map<MovieResponse, number>(({ totalResults }: MovieResponse) =>
+                totalResults !== undefined ? totalResults : 0
+            )
+        );
+    }
+
+    set serachTerm(term: String) {
+        if (this.searchTermBS.value != term) {
+            this.actualPageBS.next(0);
+        }
+        this.searchTermBS.next(term);
+    }
+
+    set changePage(page: number) {
+        this.actualPageBS.next(page);
+    }
+
+    private getUrl(params: any) {
+        const [searchTerm, actualPage] = params;
+        const term = searchTerm || DEFAULT_SEARCH_TERM;
+
+        return `${API_URL_BASE}?apikey=${API_KEY}&page=${
+            actualPage + 1
+        }&s=${term}`;
+    }
 }
