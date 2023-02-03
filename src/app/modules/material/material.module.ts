553cd43d0a9aa413d721c63cf2731cc2d3e9fb0d
Fabian Forero <fabianforero779@gmail.com>
2023-02-02 23:34:21 -0500

    Add: Including new modules in Mterial MOdule
    include => MatChipsModule.
               MatDividerModule

--- src/app/modules/material/material.module.ts
+++ src/app/modules/material/material.module.ts
@@ -4,7 +4,9 @@ import { CommonModule } from '@angular/common';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
 import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatChipsModule } from '@angular/material/chips';
 import { MatDialogModule } from '@angular/material/dialog';
+import { MatDividerModule } from '@angular/material/divider';
 import { MatIconModule } from '@angular/material/icon';
 import { MatInputModule } from '@angular/material/input';
 import { MatFormFieldModule } from '@angular/material/form-field';
@@ -17,7 +19,9 @@ const MATERIAL_MODULES = [
   MatButtonModule,
   MatCardModule,
   MatCheckboxModule,
+  MatChipsModule,
   MatDialogModule,
+  MatDividerModule,
   MatIconModule,
   MatInputModule,
   MatFormFieldModule,
