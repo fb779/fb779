da423df92350c774724d4b666045d55cbae836ef
Fabian Forero <fabianforero779@gmail.com>
2023-04-16 23:09:04 -0400

    Mod: Adjusted heroe.service.
    Define the urlApi out of the class as a const varaible.
    
    Delete variables DATA_LIST and listBS, it doesn't necessary.
    
    Create heroResponse observable to make request call,
      and modified the listHeroes getter to return the heroesRespont observable.

--- src/app/modules/heroes/services/heroe.service.ts
+++ src/app/modules/heroes/services/heroe.service.ts
@@ -1,52 +1,43 @@
-import { Injectable, Pipe } from '@angular/core';
+import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { BehaviorSubject, map, Observable, Subject, tap } from 'rxjs';
+import { catchError, Observable, of, Subject } from 'rxjs';
 import { Hero } from '../models/hero';
-// import { DATA_LIST } from '../models/data';
 
-import { StoreService } from './store.service';
+const urlApi = `https://akabab.github.io/superhero-api/api/all.json`;
 
 @Injectable({
   providedIn: 'root',
 })
 export class HeroeService {
-  private DATA_LIST: Hero[] = [];
-  private listBS: BehaviorSubject<Hero[]> = new BehaviorSubject(this.DATA_LIST);
-  // private heroBS: BehaviorSubject<Hero> = new BehaviorSubject(DATA_LIST[0]);
+
   private heroBS: Subject<Hero> = new Subject();
+
+  heroesRresponse$: Observable<Hero[]> = this.http.get<Hero[]>(urlApi).pipe(
+    catchError((err) => {
+      console.error(err.message)
+      return of([]);
+    })
+  );
+
+  get listHeroes$(): Observable<Hero[]> {
+    return this.heroesRresponse$
+  }
 
   get hero$(): Observable<Hero | null> {
     return this.heroBS.asObservable();
   }
 
   set heroChosing(hero: Hero | null) {
-    hero && this.heroBS.next(hero);
+    hero && this.heroBS.next(hero); // if hero is different to null they will emit the hero
   }
 
-  get listHeroes$(): Observable<Hero[]> {
-    return this.listBS.asObservable();
-  }
+  constructor(private http: HttpClient) { }
 
-  constructor(private http: HttpClient, private storeService: StoreService) {
-    this.getheroData()
-      .pipe(
-        // tap((data) => this.storeService.saveData('heroes', data)),
-        // tap((data) => console.log('data cargada', data)),
-        tap((data) => this.listBS.next(data))
-      )
-      .subscribe();
-  }
+  // getHero(id: string) { }
 
-  private getheroData(): Observable<Hero[]> {
-    const urlApi = `https://akabab.github.io/superhero-api/api/all.json`;
-    return this.http.get<Hero[]>(urlApi);
-  }
+  // saveHero(hero: Hero) { }
 
-  getHero(id: string) {}
+  // updateHero(id: string, hero: Hero) { }
 
-  saveHero(hero: Hero) {}
-
-  updateHero(id: string, hero: Hero) {}
-
-  deleteHero(id: string) {}
+  // deleteHero(id: string) { }
 }
